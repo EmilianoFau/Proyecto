1e925c6fb4f9d840545c39600113f65e78982824
clouse9 <erikfhernandezv@gmail.com>
2022-09-28 22:34:33 -0300

    Clase administrador

--- src/Library/Administrador.cs
+++ src/Library/Administrador.cs
@@ -8,21 +8,33 @@ namespace Proyecto_Chatbot
         public string Email {get; set;}
         public string Username {get; set;}
         private string Contraseña {get; set;}
-        public void CrearCategoria()
+        public Administrador(string nombre, string email, string username, string contraseña)
         {
-
+            this.Nombre = nombre;
+            this.Email = email;
+            this.Username = username;
+            this.Contraseña = contraseña;
         }
-        public void EliminarCategoria()
+        
+        public void CrearCategoria(Categoria categoria)
         {
-
+            /* Code here */
         }
-        public void EliminarOferta()
+        public void EliminarCategoria(Categoria categoria)
         {
-
+            /* Code here */
+        }
+        public void EliminarOferta(OfertaLaboral oferta)
+        {
+            /* Code here */
+        }
+        public void Registrarse(string username, string email, string password)
+        {
+            /* Code here */
         }
         public void Login()
         {
-
+            /* Code here */
         }
     }
 }
