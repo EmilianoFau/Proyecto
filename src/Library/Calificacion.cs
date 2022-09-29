a86430aa467936c2392718bf2153c95121072a92
clouse9 <erikfhernandezv@gmail.com>
2022-09-28 22:38:03 -0300

    avance clase calificacion

--- src/Library/Calificacion.cs
+++ src/Library/Calificacion.cs
@@ -2,4 +2,15 @@ using System;
 
 namespace Proyecto_Chatbot
 {
+    public class Calificacion
+    {
+        public string Descripcion{get; set;}
+        public double Puntaje {get; set;}
+
+        public Calificacion(string descripcion, double puntaje)
+        {
+            this.Descripcion = descripcion;
+            this.Puntaje = puntaje;
+        }
+    }
 }
