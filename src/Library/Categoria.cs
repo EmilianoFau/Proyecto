0dde96f3a584341ae549e362e22834758cd04dad
clouse9 <erikfhernandezv@gmail.com>
2022-09-28 22:38:40 -0300

    avance Categoria

--- src/Library/Categoria.cs
+++ src/Library/Categoria.cs
@@ -2,4 +2,15 @@ using System;
 
 namespace Proyecto_Chatbot
 {
+    public class Categoria
+    {
+        public string Nombre {get; set;}
+        public string Descripcion {get; set;}
+
+        public Categoria(string nombre, string descripcion)
+        {
+            this.Descripcion = descripcion;
+            this.Nombre = nombre;
+        }
+    }
 }
